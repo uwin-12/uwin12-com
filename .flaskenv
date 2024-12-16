6b66d19325e18738663f3e3b4d9acd853015545b
MbappeWang <109477301+mbappewang@users.noreply.github.com>
2024-12-17 00:27:11 +0800

    更新 Flask 配置，添加新的路由和应用程序文件

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,1 @@
-FLASK_ENV=development
-FLASK_APP=watchlist
+FLASK_DEBUG=1
---
+++ service/routes.py
@@ -0,0 +1,9 @@
+from app import app
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
